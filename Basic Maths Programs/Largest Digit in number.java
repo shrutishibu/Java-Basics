489c284c110912a1e51284239d9dfba613155aa7
Shruti Maria Shibu <sshrutishibu@gmail.com>
2025-08-19 17:29:59 +0530

    Largest digit in a number

---
+++ Basic Maths Programs/Largest Digit in number.java
@@ -0,0 +1,34 @@
+/*
+You are given an integer n. Return the largest digit present in the number.
+
+Examples:
+Input: n = 25
+Output: 5
+Explanation: The largest digit in 25 is 5.
+
+Input: n = 99
+Output: 9
+Explanation: The largest digit in 99 is 9.
+
+Input: n = 1
+Output: 1
+Constraints:
+0 <= n <= 5000
+n will contain no leading zeroes except when it is 0 itself.
+
+*/
+
+//Solution
+class Solution {
+    public int largestDigit(int n) {
+        if(n==0) return 0;
+
+        ArrayList<Integer> digitsOfNumber = new ArrayList<>();
+        while(n>0){
+            digitsOfNumber.add(n%10);
+            n = n/10;
+        }
+
+        return Collections.max(digitsOfNumber);
+    }
+}
